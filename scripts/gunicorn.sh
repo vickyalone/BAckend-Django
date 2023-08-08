db9cb5c049e2cfe37ed5056c422ec0480d33745a
vickyalone <106733203+vickyalone@users.noreply.github.com>
2023-08-09 04:47:47 +0530

    Update gunicorn.sh

--- scripts/gunicorn.sh
+++ scripts/gunicorn.sh
@@ -1,5 +1,19 @@
 #!/usr/bin/bash
 
-sudo cp /gunicorn/gunicorn.service  /etc/systemd/system/gunicorn.service
-sudo systemctl start gunicorn
-sudo systemctl enable gunicorn
+SERVICE_FILE_SOURCE="/gunicorn/gunicorn.service"
+SERVICE_FILE_DEST="/etc/systemd/system/gunicorn.service"
+
+# Check if the source service file exists
+if [ -e "$SERVICE_FILE_SOURCE" ]; then
+    # Copy the service file to the destination
+    sudo cp "$SERVICE_FILE_SOURCE" "$SERVICE_FILE_DEST"
+    sudo systemctl daemon-reload
+
+    # Start and enable the service
+    sudo systemctl start gunicorn
+    sudo systemctl enable gunicorn
+
+    echo "Gunicorn service started and enabled."
+else
+    echo "Source service file not found: $SERVICE_FILE_SOURCE"
+fi
