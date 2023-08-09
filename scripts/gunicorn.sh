ab94261b39dcb342b81542dd005a492de7ba41ac
Vigneshwaran Ganesan <vigneshwaran.ganesan@aspirenxt.com>
2023-08-10 01:28:18 +0530

    added frontend code

--- scripts/gunicorn.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/bash
-
-sudo cp /gunicorn/gunicorn.service  /etc/systemd/system/gunicorn.service
-sudo systemctl start gunicorn
-sudo systemctl enable gunicorn
--- scripts/python_dependencies.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-virtualenv /home/ubuntu/env
-source /home/ubuntu/env/bin/activate
-pip install -r /home/ubuntu/ci-cd-django/requirements.txt
